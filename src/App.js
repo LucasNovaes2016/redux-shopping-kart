3cfa1de5c08c792dfa6ccf237a6c0008a5b5ef60
Lucas Novaes <lucasmsn200@gmail.com>
2019-12-08 20:40:52 -0200

    removendo arquivos desnecessarios

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hello from main component </h1>
+    </div>
+  );
+}
+
+export default App;
